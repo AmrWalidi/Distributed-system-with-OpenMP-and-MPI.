76123e9f064d0c1c61e1c8c1de86f64d9f00e6d3
Amr Walidi <amr.nawaf@gmail.com>
2025-03-20 14:24:28 +0300

    add project

--- dockerfile
+++ dockerfile
@@ -1,7 +1,5 @@
-# Use an image with MPI and OpenMP support
 FROM ubuntu:latest
 
-# Install dependencies
 RUN apt-get update && apt-get install -y \
     openmpi-bin \
     openmpi-common \
@@ -11,14 +9,10 @@ RUN apt-get update && apt-get install -y \
     libomp-dev \
     && rm -rf /var/lib/apt/lists/*
 
-# Set working directory
 WORKDIR /app
 
-# Copy source files into the container
 COPY . /app
 
-# Compile the MPI + OpenMP program
 RUN mpicc -fopenmp -o distributed_program main.c
 
-# Set the command to run the MPI program
 CMD ["mpirun", "--allow-run-as-root", "-np", "2", "./distributed_program"]
--- main.c
+++ main.c
@@ -3,20 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DATA_SIZE 1000  // Size of the data to process
+#define DATA_SIZE 1000 
 
 int main(int argc, char** argv) {
     int world_rank, world_size;
 
-    // Initialize MPI
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    int chunk_size = DATA_SIZE / world_size;  // Distribute data equally
+    int chunk_size = DATA_SIZE / world_size;  
     int data[chunk_size];
 
-    // Each node processes its own chunk using OpenMP
     #pragma omp parallel for
     for (int i = 0; i < chunk_size; i++) {
         data[i] = world_rank * chunk_size + i;
